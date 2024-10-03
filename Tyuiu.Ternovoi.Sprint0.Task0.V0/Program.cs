d2879bc2b4ea214b5a807d96f2237740026fe244
danil <danil@>
2024-10-03 06:17:18 +0500

    Add project files.

---
+++ Tyuiu.Ternovoi.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.Ternovoi.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
